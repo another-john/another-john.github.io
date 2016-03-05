d63b20284aab3c138ce6cb56605b8a4c4ecd341c
Xiao Bai <Xiao Bai>
2016-03-05 19:29:01 +0800

    added artist profile

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,20 @@
 var engineerText = "Trust me, I'm an Engineer but...";
-var artistText = "I also play some music";
+var artistText = "I also play some music ;)";
 var greetingText = "Hi, I'm John and...";
+
+$(window).on("orientationchange",function(){
+setCoverPageHeight();
+bindPlayButton();
+bindScrollAction();
+});
+
+
+
+
+
 $(document).ready(function(){
 $.material.init();
- $('.chart').easyPieChart({
+ var chart = $('.chart').easyPieChart({
            animate: 2000,
 			barColor: '#69c',
 			trackColor: '#ace',
@@ -19,19 +30,21 @@ bindScrollAction();
 
 $( window ).scroll(function() {
 var scrollDistance = $(document).scrollTop();
-//console.log(scrollDistance);
-
+//console.log(scrollDistance<=$("#engineer-container").height());
 if (scrollDistance<100){
 	$("#greeting").text(greetingText);
+	$("#scrolldown").data("scrolltarget","#content-engineer");
 	
 }
-if (scrollDistance<=$(window).height() && scrollDistance>=100){
+else if (scrollDistance<=$("#engineer-container").height() && scrollDistance>=100){
 $("#greeting").text(engineerText);
-
+$("#scrolldown").data("scrolltarget","#content-artist");
 }
 	
-if (scrollDistance>$(window).height())
+else {
 	$("#greeting").text(artistText);
+	$("#scrolldown").data("scrolltarget","#content-engineer");
+}
 
 });
 
@@ -64,9 +77,14 @@ $("#video-wrapper").show();
 function setCoverPageHeight(){
 var windowHeight = $(window).height();
 var windowWidth = $(window).width();
-var img = document.getElementById('profile-engineer'); 
-var imgHeight = img.clientHeight;
-$("#content-engineer").css("height",0.7*imgHeight);
+var imgEngineer = document.getElementById('profile-engineer'); 
+var imgEngineerHeight = imgEngineer.clientHeight;
+$("#content-engineer").css("height",imgEngineerHeight);
+var imgArtist = document.getElementById('profile-artist'); 
+var imgArtistHeight = imgEngineer.clientHeight;
+$("#content-artist").css("height",imgArtistHeight);
+
+
 
 if ($(".content").length)
 {
